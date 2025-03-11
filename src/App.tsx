149af9eea5bb68ce6d48650568aba6b6fa626d54
Rajesh Kumar <rakumar@q3infotech.net>
2025-03-11 17:48:05 +0530

    Created new styles3 file for scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Box, Stack } from '@mui/material';
 import "./assets/css/style.scss";
+import "./assets/css/style3.scss";
 import NoDataStoresFound from './frontend/components/datastores/noDataStoresFound';
 import Sidebar from './frontend/components/common/sidebar';
 function App() {
